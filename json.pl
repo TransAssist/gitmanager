315fd166aeebaf65a1caa99a0c78deb53a9e9b1c
onoie <onoie3@gmail.com>
2017-09-08 09:54:52 +0900

    fast commit

---
+++ json.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+
+use JSON;
+
+my $json_data = '[{"name":"foo","id":"100"},{"name":"bar","id":"110"},{"name":"baz","id":"120"}]';
+my $items = JSON->new()->decode($json_data);
+
+foreach my $item (@$items) {
+    printf "%s,%s\n", $item->{name}, $item->{id};
+}
